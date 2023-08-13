abe4d7348be809264f738cc49e03cc03ad504c43
Alexey2001 <101454253+Alexey2001@users.noreply.github.com>
2023-08-13 14:08:41 +0300

    Create RotatieStatieSpatiala.cs

---
+++ RotatieStatieSpatiala.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatieStatieSpatiala : MonoBehaviour
+{
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, 0, 10) * Time.deltaTime);
+
+    }
+}
+
+
